47775153fcb4e4484e19a012c82230c67f8f139c
Hudson H <Hudsonhargrove01@gmail.com>
2025-02-21 02:58:12 -0600

    Refactor user interaction in main script for Sudoku puzzle generation

--- sudoku-generator/src/__init__.py
+++ sudoku-generator/src/__init__.py
@@ -9,28 +9,32 @@ if __name__ == "__main__":
     start_overall_time = time.time()
     board = generator.generate_puzzle(30)
     start_instace_time = time.time()
-    while solver.solve(board):
+    while True:
 
         board = generator.generate_puzzle(30)
         attempts += 1
         end_instance_time = time.time()
         print(f"Time taken for instance {attempts}: {end_instance_time - start_instace_time:.2f} seconds")
-        
 
-        if attempts > 1000 or input("Do you want to continue? (y/n): ").lower() == "n":
+        if attempts > 1000:
             print(f"Failed to generate invalid puzzle at {attempts} attempts")
             end_overall_time = time.time()
             print(f"Time taken: {end_overall_time - start_overall_time:.2f} seconds")
-            break
-        elif input("Do you want to continue? (y/n): ").lower() == "y":
+            exit()
+
+        usr = input("Do you want to continue? (y/n): ").lower()
+
+        if usr == "n":
+            print("Completed puzzle:")
+            print(board)
+            end_overall_time = time.time()
+            print(f"Total time taken: {end_overall_time - start_overall_time:.2f} seconds")
+            exit()
+        elif usr =="y":
             print(f"Puzzle generation attempt {attempts} successful")
             end_overall_time = time.time()
             start_instace_time = time.time()
             print(f"Total time taken: {end_overall_time - start_overall_time:.2f} seconds")
         else:
             print("Invalid input. Try again.")
-    print("Unsolvable puzzle generated")
-    print(board)
-    end_overall_time = time.time()
-    print(f"Total time taken: {end_overall_time - start_overall_time:.2f} seconds") 
             
